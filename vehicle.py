c9d7a50c5f56b33e62dc341631ec41775e3dedd1
Blake Lockley <bwlockley@gmail.com>
2019-03-07 15:07:52 +1100

    Changed behaviour of the Vehicle class

--- vehicle.py
+++ vehicle.py
@@ -5,16 +5,17 @@
 #   - Velocity: The speed and direction (+/-) it is travelling 
 
 # The Vehicle can be be given one of two intructions:
-#   - Accelerate (A): The speed of the vehicle doubles
-#   - Reverse (R): The speed of the vehicle is reversed
+#   - Accelerate (A): The speed of the vehicle increases by 1
+#   - Reverse (R): The vehicle comes to a hault and the direction of the vehicle is reversed
 
-# Every time an before instruction is read, the position of
+# Every time an instruction is read, the position of
 #   the vechile will be updated by the current velocity
 
 
 class Vehicle:
-    _position = 0
-    _velocity = 1
+    _position  = 0
+    _velocity  = 0
+    _direction = 1
 
     @property
     def position(self):
@@ -25,16 +26,17 @@ class Vehicle:
         return self._velocity
 
     def read_instruction(self, inst):
-        self._position += self._velocity
-
         if inst == 'A':
-            self._velocity *= 2
+            self._velocity += self._direction
 
         elif inst == 'R':
-            self._velocity *= -1
+            self._velocity = 0
+            self._direction *= -1
 
         else:
             raise ValueError("Unexpected instruction.")
+
+        self._position += self._velocity
 
 
     def read_string(self, string):
